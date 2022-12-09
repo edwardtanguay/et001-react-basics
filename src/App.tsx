94476b2033669e2fbfd663244aa29980bd2487ef
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-09 13:48:37 +0100

    reading data from json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.scss';
+import nouns from './data/nouns.json';
 
-function App() {
-  return <div className="App">
-    <h1>React Basics</h1>
-    <p>These are the basics.</p>
-  </div>;
-}
+const App = () => {
+	const title = 'The React Basics';
+
+	return (
+		<div className="App">
+      <h1>{title}</h1>
+			<p>These are the basics.</p>
+      <h2>German Nouns</h2>
+      <p>There are {nouns.length} nouns:</p>
+      <div className="nouns">
+        {nouns.map(noun => {
+          return (
+            <div className="noun">{noun.article} {noun.singular}</div>
+          )
+        })}
+      </div>
+		</div>
+	);
+};
 
 export default App;
