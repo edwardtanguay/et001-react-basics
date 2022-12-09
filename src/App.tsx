6bd00256b3ef74522d83a5cbe1f585d1ea593c5f
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-09 13:50:39 +0100

    nouns with styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,24 @@ const App = () => {
 
 	return (
 		<div className="App">
-      <h1>{title}</h1>
+			<h1>{title}</h1>
 			<p>These are the basics.</p>
-      <h2>German Nouns</h2>
-      <p>There are {nouns.length} nouns:</p>
-      <div className="nouns">
-        {nouns.map(noun => {
-          return (
-            <div className="noun">{noun.article} {noun.singular}</div>
-          )
-        })}
-      </div>
+			<h2>German Nouns</h2>
+			<p>There are {nouns.length} nouns:</p>
+			<div className="nouns">
+				{nouns.map((noun) => {
+					return (
+						<div className="noun">
+							<div className="line1">
+								{noun.article} {noun.singular}
+							</div>
+              <div className="line2">
+                {noun.plural}
+              </div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
