dccbaaba3c2e231881e2ee3b6d710c33a8cbc55e
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-09 14:58:14 +0100

    front/back

--- src/components/Noun.tsx
+++ src/components/Noun.tsx
@@ -8,10 +8,15 @@ export const Noun = (props: IProps) => {
 	const { noun } = props;
 	return (
 		<div className="noun">
-			<div className="line1">
-				{noun.article} {noun.singular}
+			<div className="front">
+				{noun.singular}
 			</div>
-			<div className="line2">{noun.plural}</div>
+			<div className="back">
+				<div>
+					{noun.article} {noun.singular}
+				</div>
+				<div>{noun.plural}</div>
+			</div>
 		</div>
 	);
 };
